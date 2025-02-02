5ebbbf22a4e3a02c0dd9ced533f4f2b9661dce93
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-02 10:49:37 -0300

    agregando e importando componentes en app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'api-project';
