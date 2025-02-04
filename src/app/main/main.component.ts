f778911fb4fc287326763b85171e2b331d99c3ef
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-04 14:15:11 -0300

    font awesome instalado

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { NgClass, NgFor, NgIf } from '@angular/common';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main',
