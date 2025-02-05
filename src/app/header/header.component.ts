cea44aab48a0daec12692314b82bf61019f492ff
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-05 14:48:57 -0300

    agregando audio a header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor(private library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
 
+  playAudio() {
+    const audioPlayer = document.getElementById(
+      'audioPlayer'
+    ) as HTMLAudioElement;
+    if (audioPlayer) {
+      audioPlayer.play();
+    }
+  }
+
+  pauseAudio() {
+    const audioPlayer = document.getElementById(
+      'audioPlayer'
+    ) as HTMLAudioElement;
+    if (audioPlayer) {
+      audioPlayer.pause();
+    }
+  }
 }
