86bb27fcbf7fa6deb2bbc5ff281ea966a10e2b8a
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-06 10:33:52 -0300

    CommonModule importado e implementado en header component ts para el correcto renderizado dinamico de los iconos activar/mutear sonido

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -6,7 +7,7 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
